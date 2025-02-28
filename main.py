ef912bde2e4621f22d418ac7d7307b719c396fc8
Rodgers31 <otienor986@gmail.com>
2025-02-27 22:55:57 -0600

    cars detection

--- main.py
+++ main.py
@@ -21,10 +21,15 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
     car.create_car()
     car.move_cars()
 
+#     Detect collision with car
+    for one_car in car.all_cars:
+        if one_car.distance(player) < 20:
+            game_is_on = False
 
-
+#
 
 
